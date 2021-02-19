f848390f86bde85d480b08a6f254456933afc39b
Richard <>
2021-02-19 17:12:33 -0500

    added some sliders to move the image up and down

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -11,26 +11,49 @@ class ViewController: UIViewController {
 
     @IBOutlet var myImage : UIImageView!
     @IBOutlet var myButton : UIButton!
+    @IBOutlet var locationXSlider : UISlider!
+    @IBOutlet var locationYSlider : UISlider!
+    @IBOutlet var XLabel : UILabel!
+    @IBOutlet var YLabel : UILabel!
+
     
     var pressedOnce = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        locationXSlider
+        
     }
 
 
-    @IBAction func clickedButton(sender: UIButton){
+    @IBAction func clickedButton(_ sender: UIButton){
         if(!pressedOnce){
-            myImage.alpha = 1
-            sender.setTitle("Respond?", for: .normal)
+            locationXSlider.isEnabled = true
+            locationXSlider.value = 50.0
+            locationYSlider.isEnabled = true
+            locationYSlider.value = 50.0
+            XLabel.isEnabled = true
+            YLabel.isEnabled = true
+            myImage.isHidden = false
+            myButton.setTitle("Respond?", for: .normal)
             pressedOnce = true
         }
         else{
             myImage.image = UIImage(named: "general_kenobi.jpg")
-            //sender.delete(Any?.self)
+            myButton.isEnabled = false
         }
         
+    }
+    
+    @IBAction func moveXSlider(_ sender: UISlider){
+        myImage.center.x = 135.0 + CGFloat(locationXSlider.value)
+        XLabel.text = String(locationXSlider.value)
+    }
+    
+    @IBAction func moveYSlider(_ sender: UISlider){
+        myImage.center.y = 135.0 + CGFloat(locationYSlider.value)
+        YLabel.text = String(locationYSlider.value)
     }
     
 }
