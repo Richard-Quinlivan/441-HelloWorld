2fdb9b6bf2f587fbd276d08afd772ee2969c1272
Richard <>
2021-02-18 21:47:16 -0500

    added images and code that lets you press a button to display them

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myImage : UIImageView!
+    @IBOutlet var myButton : UIButton!
+    
+    var pressedOnce = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func clickedButton(sender: UIButton){
+        if(!pressedOnce){
+            myImage.alpha = 1
+            sender.setTitle("Respond?", for: .normal)
+            pressedOnce = true
+        }
+        else{
+            myImage.image = UIImage(named: "general_kenobi.jpg")
+            //sender.delete(Any?.self)
+        }
+        
+    }
+    
 }
 
